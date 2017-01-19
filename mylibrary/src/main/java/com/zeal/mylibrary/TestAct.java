faf705666a1ffc0591177b0a15b402c0c367e6a7
liaowj <lwj_zeal@163.com>
2017-01-19 15:56:05 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/zeal/mylibrary/TestAct.java
@@ -0,0 +1,15 @@
+package com.zeal.mylibrary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+public class TestAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        RecyclerView rv = new RecyclerView(this);
+    }
+}
